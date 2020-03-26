d93dbda5cdad01ff05724f2da0ed8ea8ccb9e103
Matheus Ishiyama <semrumo3@hotmail.com>
2020-03-25 21:38:24 -0300

    front-end da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-
 import React from 'react';
+import './global.css'
+import Routes from './routes.js'
 
-function App() {
+export default function App() {
   return (
-    <h1>Hello world</h1>
+    <Routes />
   );
 }
-
-export default App;
